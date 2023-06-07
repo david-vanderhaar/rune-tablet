6e38013636f3961d13742f60583e3c5faee42739
David Vanderhaar <dvanderhaarhunter@bamboohealth.com>
2023-06-07 16:37:33 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rune Tablet
+Experience the RUNE RPG on this magical tablet they call The Browser
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { base } from "$app/paths";
+</script>
+
+
+<header>
+  <h1>RUNE Tablet</h1>
+  <p>Play RUNE RPG in the browser</p>
+</header>
+<slot></slot>
+
