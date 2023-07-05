cb4ec45d51ff537b7530333f3a82495d9163d809
David Vanderhaar <d.vanderhaarhunter@gmail.com>
2023-07-05 14:16:37 -0400

    disable localstorage for ssr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
   import { fade } from 'svelte/transition';
   import { base } from "$app/paths";
-
 </script>
 <div transition:fade class='container'>
   <p>This is a set of tools to help creators enjoy and develop third-party content for RUNE, <a target="_blank" href="https://gilarpgs.itch.io/rune">"a solo tabletop RPG inspired by the soulslike genre of video games"</a>.</p>
--- src/utils/PersistentStore.js
+++ src/utils/PersistentStore.js
@@ -3,13 +3,29 @@
 // 1. a key to use for the local storage
 // 2. a default value to use if the local storage is empty
 import { writable } from 'svelte/store'
+import { browser } from '$app/environment';
 
 export const createPersistentStore = (key, defaultValue) => {
+  if (!browser) return createSsrWritable(defaultValue);
+  return createBrowserWritable(key, defaultValue);
+}
+
+const createBrowserWritable = (key, defaultValue) => {
   const stored = localStorage.getItem(key)
   const { subscribe, set, update } = writable(JSON.parse(stored) || defaultValue)
   subscribe((value) => localStorage.setItem(key, JSON.stringify(value)))
   return {
     subscribe,
+    add: (item) => update((items) => [...items, item]),
+  }
+}
+
+const createSsrWritable = (defaultValue) => {
+  const { subscribe, set, update } = writable(defaultValue)
+  return {
+    subscribe,
+    set,
+    update,
     add: (item) => update((items) => [...items, item]),
   }
 }
